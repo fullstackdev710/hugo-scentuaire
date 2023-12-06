406494e1308329b86d3fdcd9e7bf3ed15fc3d123
Peace Marketing <fullstack710dev@gmail.com>
2023-12-06 13:36:06 -0400

    dfd

---
+++ generate.sh
@@ -0,0 +1,1 @@
+clear && rm -rf /home/flewid/Documents/scentuairecom_out/* && hugo -d /home/flewid/Documents/scentuairecom_out --minify && echo "done!"
--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,44 @@
 module.exports = {
+  plugins: {
+      '@fullhuman/postcss-purgecss': {
+          content: [
+              './themes/{insert_theme_name_here}/layouts/**/*.html', 
+              './themes/{insert_theme_name_here}/assets/js/*.js',
+              './themes/{insert_theme_name_here}/static/js/*.js',
+              './layouts/**/*.html',
+              './static/js/*.js'
+            ],
+          whitelist: [
+              'highlight',
+              'language-bash',
+              'pre',
+              'video',
+              'code',
+              'content',
+              'h3',
+              'h4',
+              'ul',
+              'li'
+          ]
+      },
+      autoprefixer: {},
+      cssnano: {preset: 'default'}
+  }
+};
+
+
+
+const purgecss = require("@fullhuman/postcss-purgecss")({
+  content: ["./hugo_stats.json"],
+  defaultExtractor: (content) => {
+    const els = JSON.parse(content).htmlElements;
+    return [...(els.tags || []), ...(els.classes || []), ...(els.ids || [])];
+  },
+  safelist: [],
+});
+
+module.exports = {
   plugins: [
-    require("autoprefixer")({
-      overrideBrowserslist: ["> 0.5% in US", "Safari > 9"]
-    })
-  ]
+    ...(process.env.HUGO_ENVIRONMENT === "production" ? [purgecss] : []),
+  ],
 };
